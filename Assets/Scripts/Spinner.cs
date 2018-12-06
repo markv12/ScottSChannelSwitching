2c6c58b4641985b1c4b9bafca7eb23971094596a
Scott <markv12@users.noreply.github.com>
2018-12-07 01:24:01 +0900

    Add bass pulse

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+    [SerializeField] Transform t;
+    [SerializeField] float speed = 0;
+	void Update () {
+        t.Rotate(new Vector3(0, speed, 0));
+	}
+}
